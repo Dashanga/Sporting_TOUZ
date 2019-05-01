5798131e2b83c631fb7e9d8e476b70c0ba48c6c4
Dashanga <kireytatyana@yandex.ru>
2019-05-01 15:01:13 +0200

    15:01

---
+++ Sporting/Sporting/Welcome.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Sporting
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.BackColor = System.Drawing.Color.Transparent;
+            this.labelWelcome.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWelcome.ForeColor = System.Drawing.Color.Black;
+            this.labelWelcome.Location = new System.Drawing.Point(12, -3);
+            this.labelWelcome.MaximumSize = new System.Drawing.Size(520, 0);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(503, 62);
+            this.labelWelcome.TabIndex = 0;
+            this.labelWelcome.Text = "Добро пожаловать в программу для поиска спортивной секции для ребёнка. \r\n";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.ForeColor = System.Drawing.Color.Black;
+            this.buttonStart.Location = new System.Drawing.Point(187, 62);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(130, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Начать";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(508, 325);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelWelcome);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "Welcome";
+            this.Text = "Добро пожаловать!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
