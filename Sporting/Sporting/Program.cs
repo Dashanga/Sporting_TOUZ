4f18314b84f347096833a0b097ad1c8beb147f3e
psphmarina <north.pole@list.ru>
2019-06-03 18:08:40 +0400

    Revert "4"
    This reverts commit 6c579cfe528b6071704f46153bb09e620dc1ac75.

--- Sporting/Sporting/Program.cs
+++ Sporting/Sporting/Program.cs
@@ -1,12 +1,8 @@
-﻿using SportingImplement;
-using SportServiceDAL.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Unity;
-using Unity.Lifetime;
 
 namespace Sporting
 {
@@ -18,17 +14,9 @@ namespace Sporting
         [STAThread]
         static void Main()
         {
-            var container = BuildUnityContainer();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(container.Resolve<FormWelcome>());
-        }
-        public static IUnityContainer BuildUnityContainer()
-        {
-            var currentContainer = new UnityContainer();
-            currentContainer.RegisterType<ISectionService, SectionImplement>(new
-           HierarchicalLifetimeManager());
-            return currentContainer;
+            Application.Run(new FormWelcome());
         }
     }
 }
