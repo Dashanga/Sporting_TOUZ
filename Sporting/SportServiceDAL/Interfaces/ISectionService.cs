fd4cd24e2ad81f963596df5cdbd6a7c2540be2ca
psphmarina <north.pole@list.ru>
2019-06-03 17:16:48 +0400

    3

---
+++ Sporting/SportServiceDAL/Interfaces/ISectionService.cs
@@ -0,0 +1,15 @@
+﻿using SportServiceDAL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportServiceDAL.Interfaces
+{
+    public interface ISectionService
+    {
+        List<SectionViewModel> GetList();
+        SectionViewModel GetElement(int id);
+    }
+}
---
+++ Sporting/SportingImplement/DataListSingleton.cs
@@ -0,0 +1,26 @@
+﻿using SportModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportingImplement
+{
+    class DataListSingleton
+    {
+        private static DataListSingleton instance;
+        public List<Section> Sections { get; set; }        private DataListSingleton()
+        {
+            Sections = new List<Section>();
+        }
+        public static DataListSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DataListSingleton();
+            }
+            return instance;
+        }
+    }
+}
