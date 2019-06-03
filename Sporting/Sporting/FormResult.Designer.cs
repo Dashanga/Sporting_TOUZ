1d737681be8ec9fb0b2a85a0c97395ae45921722
psphmarina <north.pole@list.ru>
2019-06-03 21:38:55 +0400

    6

--- Sporting/Sporting/FormResult.Designer.cs
+++ Sporting/Sporting/FormResult.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonEnd = new System.Windows.Forms.Button();
+            this.buttonGetData = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,11 +52,22 @@
             this.buttonEnd.UseVisualStyleBackColor = true;
             this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
             // 
+            // buttonGetData
+            // 
+            this.buttonGetData.Location = new System.Drawing.Point(13, 400);
+            this.buttonGetData.Name = "buttonGetData";
+            this.buttonGetData.Size = new System.Drawing.Size(118, 26);
+            this.buttonGetData.TabIndex = 2;
+            this.buttonGetData.Text = "Отобразить";
+            this.buttonGetData.UseVisualStyleBackColor = true;
+            this.buttonGetData.Click += new System.EventHandler(this.buttonGetData_Click);
+            // 
             // FormResult
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 427);
+            this.Controls.Add(this.buttonGetData);
             this.Controls.Add(this.buttonEnd);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FormResult";
@@ -69,5 +81,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.Button buttonGetData;
     }
 }
--- Sporting/Sporting/FormResult.cs
+++ Sporting/Sporting/FormResult.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,38 @@ namespace Sporting
 {
     public partial class FormResult : Form
     {
-        public FormResult()
+        String str;
+        public FormResult(String zapros)
         {
             InitializeComponent();
+            str = zapros;
+            //LoadData(zapros);
         }
+
 
         private void buttonEnd_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-         
+
+        private void buttonGetData_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=HOME-PC;Initial Catalog=SportSections;Integrated Security=True");
+            con.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter(Createzapros(), con);
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private String Createzapros()
+        {
+            str = str + ";";
+            return str;
+        }
     }
 }
